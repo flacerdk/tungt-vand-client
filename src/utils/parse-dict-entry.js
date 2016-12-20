f019e2962e621a3cd132fc7a0888bb6bdc59e190
Felipe Lacerda <fegolac@gmail.com>
2016-12-20 14:31:00 +0100

    Use relative path to fetch entries

--- src/utils/parse-dict-entry.js
+++ src/utils/parse-dict-entry.js
@@ -2,7 +2,7 @@ import queryString from 'query-string'
 
 export default function fetchAndParse(options) {
   const query = queryString.stringify(options)
-  return fetch(`http://127.0.0.1:3000/ddo?${query}`)
+  return fetch(`/ddo?${query}`)
     .then(response => response.text())
     .catch(ex => console.log(ex))
 }
