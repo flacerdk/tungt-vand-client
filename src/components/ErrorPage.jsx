41c81a79eb78bda1f4b40ae42a68021002475342
Felipe Lacerda <fegolac@gmail.com>
2017-01-11 11:16:49 +0100

    Add style to error page

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -3,11 +3,14 @@ import React from 'react'
 require('../stylesheets/ErrorPage.scss')
 
 export default function ErrorPage() {
-  const title = (<h1>Ingen resultater!</h1>)
   return (
     <div className="error-panel">
-      {title}
-      Vi kunne ikke finde noget. Prøv igen!
+      <div className="header">
+        <h3>Ingen resultater!</h3>
+      </div>
+      <div className="body">
+        Vi kunne ikke finde noget. Prøv igen!
+      </div>
     </div>
   )
 }
